aa462c3daf620e764e163030eb260149f3d3e921
Magdalena <Magdalena@MagdalenaDome.com>
2022-10-06 20:55:10 -0300

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inicio', url: '/folder/Inicio', icon: 'mail' },
+    { title: 'Mensajes', url: '/folder/Mensajes', icon: 'paper-plane' },
+    { title: 'Favoritos', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Cuenta', url: '/folder/Cuenta', icon: 'cog' },
+    { title: 'Nosotras', url: '/folder/Nosotras', icon: 'person' },
+    { title: 'Ayuda', url: '/folder/Ayuda', icon: 'alert' },
+    { title: 'Login', url: '/folder/login', icon: 'person' },
+    { title: 'Registro', url: '/folder/registro', icon: 'person' },
+  ];
+
+}
